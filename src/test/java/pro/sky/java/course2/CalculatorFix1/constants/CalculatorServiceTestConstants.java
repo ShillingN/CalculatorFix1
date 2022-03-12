8b74a51134ecb388ef9d2893b60a82550f022ee2
pro100 <pro100tkach@mail.ru>
2022-03-12 15:19:32 +0300

    Is done 1

---
+++ src/test/java/pro/sky/java/course2/CalculatorFix1/constants/CalculatorServiceTestConstants.java
@@ -0,0 +1,21 @@
+package pro.sky.java.course2.CalculatorFix1.constants;
+
+public class CalculatorServiceTestConstants {
+    public static final int A = 1;
+    public static final int B = -3;
+    public static final int C = 4;
+    public static final int D = 6;
+    public static final int E = 0;
+    public static final int RESULT_PLUS_A_B = -2;
+    public static final int RESULT_PLUS_C_B = 1;
+    public static final int RESULT_PLUS_D_E = 6;
+    public static final int RESULT_MINUS_A_B = 4;
+    public static final int RESULT_MINUS_C_B = 7;
+    public static final int RESULT_MINUS_D_E = 6;
+    public static final int RESULT_MULTIPLY_A_B = -3;
+    public static final int RESULT_MULTIPLY_D_E = 0;
+    public static final int RESULT_MULTIPLY_D_B = -18;
+    public static final int RESULT_DIVIDE_D_B = -2;
+    public static final int RESULT_DIVIDE_D_A = 6;
+    public static final int RESULT_DIVIDE_C_A = 4;
+}
